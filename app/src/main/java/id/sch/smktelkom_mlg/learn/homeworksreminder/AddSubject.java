0a1032c59f73556ebcc9413cea7e610d0a57284d
AinunNisaa <ainun.nizza27@gmail.com>
2016-11-07 10:33:45 +0700

    Button fungtion and erorr validation

--- app/src/main/java/id/sch/smktelkom_mlg/learn/homeworksreminder/AddSubject.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/homeworksreminder/AddSubject.java
@@ -1,9 +1,19 @@
 package id.sch.smktelkom_mlg.learn.homeworksreminder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddSubject extends AppCompatActivity {
+
+    EditText etSubject, etTeacher;
+    Button buttonSave;
+    TextView tvhasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +22,44 @@ public class AddSubject extends AppCompatActivity {
 
         setTitle("Add Subject");
 
+        etSubject = (EditText) findViewById(R.id.editTextSubject);
+        etTeacher = (EditText) findViewById(R.id.editTextTeacher);
+        tvhasil = (TextView) findViewById(R.id.tvHasil);
+        buttonSave = (Button) findViewById(R.id.buttonSave);
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Button OK KLIK!
+                if (etSubject.getText().toString().isEmpty()) {
+                    etSubject.setError("Subject is empty!");
+                } else if (etTeacher.getText().toString().isEmpty()) {
+                    etTeacher.setError("Teacher is empty!");
+                } else {
+                    doKlik();
+                    Toast.makeText(getApplicationContext(), "Subject has been added success!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void doKlik() {
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Subject : ").append(etSubject.getText()).append("\n");
+        builder.append("Teacher : ").append(etTeacher.getText()).append("\n");
+
+        tvhasil.setText(builder);
     }
 }
